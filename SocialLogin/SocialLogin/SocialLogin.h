7591be61c371a2ac4b1b954d42dbcb317e397e6b
Rahul-iOS-ttn <rahul.sharma1@tothenew.com>
2021-06-23 15:23:40 +0530

    added framework

---
+++ SocialLogin/SocialLogin/SocialLogin.h
@@ -0,0 +1,18 @@
+//
+//  SocialLogin.h
+//  SocialLogin
+//
+//  Created by TTN on 23/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocialLogin.
+FOUNDATION_EXPORT double SocialLoginVersionNumber;
+
+//! Project version string for SocialLogin.
+FOUNDATION_EXPORT const unsigned char SocialLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialLogin/PublicHeader.h>
+
+
